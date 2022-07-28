4fe6c7a80e525a848619873528f379e8c8b6a231
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-28 22:35:52 +0530

    using material ui icons sidebar header part is done

--- whatsapp_frontend/src/Sidebar.js
+++ whatsapp_frontend/src/Sidebar.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import "./Sidebar.css";
+import  DonutLargeIcon  from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
   return (
-    <div>
-      
+    <div className="sidebar">
+        <div className='sidebar_header'>
+        <Avatar src="https://tse3.mm.bing.net/th?id=OIP.OJMH1q9cS-AFG308T5O_PQHaLH&pid=Api&P=0"/>
+      <div className='sidebar_headerRight'>
+        <IconButton>
+        <DonutLargeIcon />
+        </IconButton>
+        <IconButton>
+        <ChatIcon />
+        </IconButton>
+        <IconButton>
+        <MoreVertIcon />
+        </IconButton>
+      </div>
+     </div> 
     </div>
   )
 }
