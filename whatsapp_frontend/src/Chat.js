acf16a041a37ed5e0614d080149b99aee46df90c
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-28 23:05:14 +0530

    chat header completed successfully using material ui icons

--- whatsapp_frontend/src/Chat.js
+++ whatsapp_frontend/src/Chat.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import "./Chat.css";
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
   return (
-    <div>
-      
+    <div className="chat">
+       <div className="chat_header">
+        <Avatar />
+
+        <div className="chat_headerInfo">
+            <h3>Room name</h3>
+            <p>Last seen at...</p>
+        </div>
+
+        <div className='chat_headerRight'>
+        <IconButton>
+        <SearchOutlined />
+        </IconButton>
+        <IconButton>
+        <AttachFile />
+        </IconButton>
+        <IconButton>
+        <MoreVert />
+        </IconButton>
+        </div>
+      </div>
+
+
+
     </div>
   )
 }
