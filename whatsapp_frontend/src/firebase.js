a3b68c75317a81546223278079a08163a9068276
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-28 22:28:53 +0530

    project initialised with installation of all necessary dependencies & for frontend setup created 2 components sidebar & chat

---
+++ whatsapp_frontend/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNDieWWvI7SJA3KU5Itu0IXNzno96Lajw",
+    authDomain: "whatsapp-project-e9e42.firebaseapp.com",
+    projectId: "whatsapp-project-e9e42",
+    storageBucket: "whatsapp-project-e9e42.appspot.com",
+    messagingSenderId: "309589216040",
+    appId: "1:309589216040:web:b63d71118000bf7f151e1d",
+    measurementId: "G-DFQ5TQRJQZ"
+  };
