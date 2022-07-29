424f91604f54292456c66cc5f91dbe69a375e1aa
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-29 17:40:07 +0530

    use pusher in the frontend and succesfully able to get realtime chat using mongodb

--- whatsapp_frontend/src/App.js
+++ whatsapp_frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('4002a38c6da70606be94', {
+      cluster: 'ap2'
+    });
+  
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app_body">
