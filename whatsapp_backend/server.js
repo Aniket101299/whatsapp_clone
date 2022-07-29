c90592a6cebfe97aed62c75516ba5bf15da1578a
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-07-29 15:55:01 +0530

    created get and post routes for the messages and install pusher to make mongodb realtime

--- whatsapp_backend/server.js
+++ whatsapp_backend/server.js
@@ -1,17 +1,28 @@
 // importing 
 import express from 'express';
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 // app config
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5678;
+
+const pusher = new Pusher({
+    appId: "1452097",
+    key: "4002a38c6da70606be94",
+    secret: "e0d0c8e8bfb29c16fdb9",
+    cluster: "ap2",
+    useTLS: true
+  });
+
 
 // middleware
+app.use(express.json());
 
 // DB config
-const connection_url = "mongodb+srv://aniket:dA9sx4TXw9imKaOt@cluster0.k6cw6vn.mongodb.net/whatsappdb?retryWrites=true&w=majority";
+const connection_url = "mongodb+srv://aniket:Aniketwhatsapp@cluster0.k6cw6vn.mongodb.net/whatsappdb?retryWrites=true&w=majority";
 mongoose.connect(connection_url, {
-    useCreteIndex: true,
+    // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -19,7 +30,30 @@ mongoose.connect(connection_url, {
 // ???
 
 // api routes
-app.get("/", (req, res) => res.status(200).send("hello world"))
+app.get("/", (req, res) => res.status(200).send("hi"));
+
+app.get("/messages/sync", (req, res) => {
+    Messages.find((err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    })
+});
+
+
+app.post("/messages/new", (req, res) => {
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err, data) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
+});
 
 // listen
 app.listen(port, ()=> console.log(`Listening on localhost: ${port}`));
